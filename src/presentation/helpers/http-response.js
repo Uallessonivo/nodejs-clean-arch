8f11d70b93da23c1b84f7a4a5a6aa3e575ea96a9
Uallessonivo <uallessons@gmail.com>
2022-07-13 08:15:14 -0300

    move production code to their own files

---
+++ src/presentation/helpers/http-response.js
@@ -0,0 +1,16 @@
+import MissingParamError from './missing-params'
+
+export default class httpResponse {
+  static badRequest (paramName) {
+    return {
+      statusCode: 400,
+      body: new MissingParamError(paramName)
+    }
+  }
+
+  static serverError () {
+    return {
+      statusCode: 500
+    }
+  }
+}
